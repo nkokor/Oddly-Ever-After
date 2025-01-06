1b3e38d5091b1e702ca63cdf6f62467027fbbb3b
nkokor <nkokor@icloud.com>
2025-01-06 22:08:26 +0100

    level up alert implemented

--- Assets/Scripts/CollisionTest.cs
+++ Assets/Scripts/CollisionTest.cs
@@ -2,37 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using UnityEngine;
+
 public class CollisionTest : MonoBehaviour
 {
-   void OnTriggerEnter(Collider other)
-    {
+    public GameObject canvas;
 
+    void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.tag == "key_1")
         {
-            PlayerPrefs.SetInt("UnlockedLevel", 2); 
-            PlayerPrefs.SetInt("CompletedLevel", 1);  
-            PlayerPrefs.Save(); 
-
-            LevelManager.Instance.LoadScene("Map", "CrossFade");
-            MusicManager.Instance.PlayMusic("Map");
-        } 
+            StartCoroutine(HandleKey("LevelUp", "Map", "Map", 2, 1));
+        }
         else if (other.gameObject.tag == "key_2")
         {
-            PlayerPrefs.SetInt("UnlockedLevel", 3); 
-            PlayerPrefs.SetInt("CompletedLevel", 2);  
-            PlayerPrefs.Save(); 
-
-            LevelManager.Instance.LoadScene("Map", "CrossFade");
-            MusicManager.Instance.PlayMusic("Map");
-        } 
+            StartCoroutine(HandleKey("LevelUp", "Map", "Map", 3, 2));
+        }
         else if (other.gameObject.tag == "key_3")
         {
-            PlayerPrefs.SetInt("UnlockedLevel", 1); 
-            PlayerPrefs.SetInt("CompletedLevel", 0);  
-            PlayerPrefs.Save(); 
-
-            LevelManager.Instance.LoadScene("HappyEnding", "CrossFade");
-            MusicManager.Instance.PlayMusic("Victory");
+            StartCoroutine(HandleKey("LevelUp", "HappyEnding", "Victory", 1, 0));
         }
     }
+
+    private IEnumerator HandleKey(string sound, string sceneName, string music, int unlockedLevel, int completedLevel)
+    {
+        canvas.SetActive(true);
+        SoundManager.Instance.PlaySound2D(sound);
+
+        PlayerPrefs.SetInt("UnlockedLevel", unlockedLevel);
+        PlayerPrefs.SetInt("CompletedLevel", completedLevel);
+        PlayerPrefs.Save();
+
+        yield return new WaitForSeconds(2.5f);
+
+        LevelManager.Instance.LoadScene(sceneName, "CrossFade");
+        MusicManager.Instance.PlayMusic(music);
+    }
 }
+
