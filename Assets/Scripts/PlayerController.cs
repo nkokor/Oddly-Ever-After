7b9e6147e8c57510921c956ff74e92e47a14e827
nkokor <nkokor@icloud.com>
2025-01-07 13:22:17 +0100

    collectibles collection fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,29 +24,30 @@ public class PlayerController : MonoBehaviour
 
     public int numberOfLives = 2;
     private float currentHealthStatus;
-    private Vector3 initialPosition; 
-    public bool isInvulnerable = false; 
+    private Vector3 initialPosition;
+    public bool isInvulnerable = false;
 
-    public TextMeshProUGUI livesText;  
-    public Slider healthSlider;  
+    public TextMeshProUGUI livesText;
+    public Slider healthSlider;
 
     private int damageTakenThisLife = 0;
 
     public Image blackoutImage;
 
     public GameObject failureCanvas;
+    public GameObject canvas;
 
     void Start()
     {
         currentSpeed = 0f;
         agent.speed = baseSpeed;
         currentHealthStatus = 1f;
-        initialPosition = new Vector3(0.03931332f, 0.328f, 1.65f);
+        initialPosition = transform.position;
 
         healthSlider.maxValue = 3;
         healthSlider.value = 3;
 
-        blackoutImage.gameObject.SetActive(false); 
+        blackoutImage.gameObject.SetActive(false);
     }
 
     void Update()
@@ -104,7 +105,7 @@ public class PlayerController : MonoBehaviour
 
     public void TriggerFall()
     {
-        if (isInvulnerable) return;  
+        if (isInvulnerable) return;
 
         animator.SetTrigger("IsFalling");
         SoundManager.Instance.PlaySound3D("Scream", transform.position);
@@ -115,39 +116,42 @@ public class PlayerController : MonoBehaviour
 
     private void TakeDamage()
     {
-        damageTakenThisLife++; 
-        if (damageTakenThisLife >= 3) {
-            currentHealthStatus = 1f; 
-            healthSlider.value = currentHealthStatus * 3; 
-        } else {
-            currentHealthStatus -= 1f / 3f; 
-            healthSlider.value = currentHealthStatus * 3; 
+        damageTakenThisLife++;
+        if (damageTakenThisLife >= 3)
+        {
+            currentHealthStatus = 1f;
+            healthSlider.value = currentHealthStatus * 3;
+        }
+        else
+        {
+            currentHealthStatus -= 1f / 3f;
+            healthSlider.value = currentHealthStatus * 3;
         }
 
         if (currentHealthStatus <= 0f)
         {
-            currentHealthStatus = 1f; 
-            healthSlider.value = currentHealthStatus * 3; 
+            currentHealthStatus = 1f;
+            healthSlider.value = currentHealthStatus * 3;
         }
 
-        healthSlider.value = currentHealthStatus * 3; 
+        healthSlider.value = currentHealthStatus * 3;
 
         if (damageTakenThisLife >= 3)
         {
-            currentHealthStatus = 1f; 
-            healthSlider.value = currentHealthStatus * 3; 
+            currentHealthStatus = 1f;
+            healthSlider.value = currentHealthStatus * 3;
             numberOfLives--;
-            damageTakenThisLife = 0; 
+            damageTakenThisLife = 0;
 
             if (numberOfLives <= 0)
             {
                 currentHealthStatus = 0;
-                healthSlider.value = currentHealthStatus * 3; 
+                healthSlider.value = currentHealthStatus * 3;
                 StartCoroutine(HandleGameOver());
             }
             else
             {
-                StartCoroutine(BlackoutAndResetPosition()); 
+                StartCoroutine(BlackoutAndResetPosition());
             }
         }
         else
@@ -159,17 +163,18 @@ public class PlayerController : MonoBehaviour
 
     private IEnumerator BlackoutAndResetPosition()
     {
-        blackoutImage.gameObject.SetActive(true); 
+        SoundManager.Instance.PlaySound2D("Life Lost");
+        blackoutImage.gameObject.SetActive(true);
 
-        blackoutImage.color = new Color(0, 0, 0, 255); 
+        blackoutImage.color = new Color(0, 0, 0, 255);
 
         float duration = 1f;
         float elapsed = 0f;
-        currentHealthStatus = 1f; 
-        healthSlider.value = currentHealthStatus * 3; 
-        
-        transform.position = initialPosition;  
-        
+        currentHealthStatus = 1f;
+        healthSlider.value = currentHealthStatus * 3;
+
+        transform.position = initialPosition;
+
         while (elapsed < duration)
         {
             blackoutImage.color = new Color(0, 0, 0, Mathf.Lerp(0, 1, elapsed / duration));
@@ -187,20 +192,20 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
 
-        blackoutImage.gameObject.SetActive(false); 
+        blackoutImage.gameObject.SetActive(false);
 
         StartCoroutine(ResetInvulnerabilityAndWait());
     }
 
     private IEnumerator HandleGameOver()
     {
-        animator.SetTrigger("IsFalling"); 
-        yield return new WaitForSeconds(2f);  
+        animator.SetTrigger("IsFalling");
+        yield return new WaitForSeconds(2f);
 
-        failureCanvas.SetActive(true);  
+        failureCanvas.SetActive(true);
         SoundManager.Instance.PlaySound2D("GameOver");
 
-        yield return new WaitForSeconds(2.5f);  
+        yield return new WaitForSeconds(2.5f);
 
         LevelManager.Instance.LoadScene("FailureScene", "CrossFade");
         MusicManager.Instance.PlayMusic("Failure");
@@ -208,7 +213,7 @@ public class PlayerController : MonoBehaviour
 
     private IEnumerator ResetInvulnerabilityAndWait()
     {
-        yield return new WaitForSeconds(4f); 
+        yield return new WaitForSeconds(4f);
         isInvulnerable = false;
     }
 
@@ -224,25 +229,80 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("extra_life"))
         {
-            SoundManager.Instance.PlaySound3D("Extra Life", transform.position);
+            Debug.Log("Picked up life elixir!");
             numberOfLives++;
+            Debug.Log($"Number of lives: {numberOfLives}");
+            livesText.text = numberOfLives.ToString();
+            Debug.Log($"Updated livesText to: {livesText.text}");
             Destroy(other.gameObject);
+            SoundManager.Instance.PlaySound3D("Extra Life", transform.position);
         }
 
         if (other.CompareTag("health_elix"))
         {
-            SoundManager.Instance.PlaySound3D("Health Elixir", transform.position);
+            Debug.Log("Picked up health elixir!");
             if (currentHealthStatus < 1f)
             {
                 currentHealthStatus += 0.33f;
+                Debug.Log($"Health increased to: {currentHealthStatus}");
                 healthSlider.value = currentHealthStatus * 3;
+                Debug.Log($"Health slider updated to: {healthSlider.value}");
                 if (currentHealthStatus > 1f) currentHealthStatus = 1f;
             }
             Destroy(other.gameObject);
+            SoundManager.Instance.PlaySound3D("Health Elixir", transform.position);
         }
 
+        if (other.CompareTag("skullcrusher"))
+        {
+            damageTakenThisLife = 3;
+            currentHealthStatus = 0f;
+            healthSlider.value = 0;
+
+            numberOfLives--;
+
+            if (numberOfLives <= 0)
+            {
+                StartCoroutine(HandleGameOver());
+            }
+            else
+            {
+                StartCoroutine(BlackoutAndResetPosition());
+                currentHealthStatus = 1f;
+                healthSlider.value = currentHealthStatus * 3;
+            }
+        }
+
+        // Collision handling for keys:
+        if (other.gameObject.CompareTag("key_1"))
+        {
+            StartCoroutine(HandleKey("LevelUp", "Map", "Map", 2, 1));
+        }
+        else if (other.gameObject.CompareTag("key_2"))
+        {
+            StartCoroutine(HandleKey("LevelUp", "Map", "Map", 3, 2));
+        }
+        else if (other.gameObject.CompareTag("key_3"))
+        {
+            StartCoroutine(HandleKey("LevelUp", "HappyEnding", "Victory", 1, 0));
+        }
     }
 
-}   
+    private IEnumerator HandleKey(string sound, string sceneName, string music, int unlockedLevel, int completedLevel)
+    {
+        canvas.SetActive(true);
+        SoundManager.Instance.PlaySound2D(sound);
+
+        PlayerPrefs.SetInt("UnlockedLevel", unlockedLevel);
+        PlayerPrefs.SetInt("CompletedLevel", completedLevel);
+        PlayerPrefs.Save();
+
+        yield return new WaitForSeconds(2.5f);
+
+        LevelManager.Instance.LoadScene(sceneName, "CrossFade");
+        MusicManager.Instance.PlayMusic(music);
+    }
+}
